11f9a4830f0112543f120b689bc06be52866891b
Edward J. Allen III <ealleniii@cpan.org>
2010-07-27 03:26:14 +0000

    Import of EALLENIII/Music-Tag-FLAC-0.4001 from CPAN.
    gitpan-cpan-distribution: Music-Tag-FLAC
    gitpan-cpan-version:      0.4001
    gitpan-cpan-path:         EALLENIII/Music-Tag-FLAC-0.4001.tar.gz
    gitpan-cpan-author:       EALLENIII
    gitpan-cpan-maturity:     released

--- lib/Music/Tag/FLAC.pm
+++ lib/Music/Tag/FLAC.pm
@@ -1,48 +1,20 @@
 package Music::Tag::FLAC;
-our $VERSION = 0.31;
+use strict;
+use warnings;
+our $VERSION = .40_01;
 
 # Copyright (c) 2007 Edward Allen III. Some rights reserved.
+
 #
-## This program is free software; you can redistribute it and/or
-## modify it under the terms of the Artistic License, distributed
-## with Perl.
+# You may distribute under the terms of either the GNU General Public
+# License or the Artistic License, as specified in the README file.
 #
 
-=pod
 
-=head1 NAME
-
-Music::Tag::FLAC - Plugin module for Music::Tag to get information from flac headers. 
-
-=head1 SYNOPSIS
-
-	use Music::Tag
-
-	my $filename = "/var/lib/music/artist/album/track.flac";
-
-	my $info = Music::Tag->new($filename, { quiet => 1 }, "FLAC");
-
-	$info->get_info();
-	   
-	print "Artist is ", $info->artist;
-
-=head1 DESCRIPTION
-
-Music::Tag::FLAC is used to read flac header information. It uses Audio::FLAC::Header. 
-
-=head1 REQUIRED VALUES
-
-No values are required (except filename, which is usually provided on object creation). 
-
-=head1 SET VALUES
-
-=cut
-
-use strict;
 use Audio::FLAC::Header;
 
 #use Image::Magick;
-our @ISA = qw(Music::Tag::Generic);
+use base qw(Music::Tag::Generic);
 
 sub flac {
 	my $self = shift;
@@ -54,26 +26,6 @@ sub flac {
 	return $self->{_Flac};
 
 }
-
-=over 4
-
-=item title, track, totaltracks, artist, album, comment, releasedate, genre, disc, label
-
-Uses standard tags for these
-
-=item asin
-
-Uses custom tag "ASIN" for this
-
-=item mb_artistid, mb_albumid, mb_trackid, mip_puid, countrycode, albumartist
-
-Uses MusicBrainz recommended tags for these.
-
-=item secs, bitrate
-
-Gathers this info from file.  Please note that secs is fractional.
-
-=cut
 
 our %tagmap = (
 	TITLE	=> 'title',
@@ -95,6 +47,14 @@ our %tagmap = (
     MUSICIP_PUID => 'mip_puid',
     MUSICBRAINZ_ALBUMARTIST => 'albumartist'
 );
+
+sub set_values {
+	return ( values %tagmap, 'picture');
+}
+
+sub saved_values {
+	return ( values %tagmap);
+}
  
 sub get_tag {
     my $self     = shift;
@@ -106,15 +66,7 @@ sub get_tag {
 			}
 		}
         $self->info->secs( $self->flac->{trackTotalLengthSeconds} );
-        $self->info->bitrate( $self->flac->{bitRate} );
-
-=pod
-
-=item picture
-
-This is currently read-only.
-
-=cut
+        $self->info->bitrate( int($self->flac->{bitRate} / 1000) );
 
 		#"MIME type"     => The MIME Type of the picture encoding
 		#"Picture Type"  => What the picture is off.  Usually set to 'Cover (front)'
@@ -150,6 +102,104 @@ sub close {
 	delete $self->{_Flac};
 }
 
+sub default_options {
+   {
+   	wav_out_system => [ "flac", "-cd", "-c", "[FILENAME]" ],
+   }
+}
+
+1;
+
+# vim: tabstop=4
+__END__
+=pod
+
+=for changes stop
+
+=head1 NAME
+
+Music::Tag::FLAC - Plugin module for Music::Tag to get information from flac headers. 
+
+=for readme stop
+
+=head1 SYNOPSIS
+
+	use Music::Tag
+
+	my $filename = "/var/lib/music/artist/album/track.flac";
+
+	my $info = Music::Tag->new($filename, { quiet => 1 }, "FLAC");
+
+	$info->get_info();
+	   
+	print "Artist is ", $info->artist;
+
+=for readme continue
+
+=head1 DESCRIPTION
+
+Music::Tag::FLAC is used to read flac header information. It uses Audio::FLAC::Header. 
+
+=begin readme
+
+=head1 INSTALLATION
+
+To install this module type the following:
+
+   perl Makefile.PL
+   make
+   make test
+   make install
+
+=head1 DEPENDENCIES
+
+This module requires these other modules and libraries:
+
+   Music::Tag
+   Audio::FLAC::Header
+
+The version info in the Makefile is based on what I use.  You can get 
+away with older versions in many cases. Do not install an older version
+of MP3::Tag.
+
+=head1 TEST FILES
+
+Test files for this module are based on the sample file for Audio::M4P.  For testing only.
+
+=end readme
+
+=for readme stop
+
+=head1 REQUIRED DATA VALUES
+
+No values are required (except filename, which is usually provided on object creation). 
+
+=head1 SET DATA VALUES
+
+=over 4
+
+=item title, track, totaltracks, artist, album, comment, releasedate, genre, disc, label
+
+Uses standard tags for these
+
+=item asin
+
+Uses custom tag "ASIN" for this
+
+=item mb_artistid, mb_albumid, mb_trackid, mip_puid, countrycode, albumartist
+
+Uses MusicBrainz recommended tags for these.
+
+=item secs, bitrate
+
+Gathers this info from file.  Please note that secs is fractional.
+
+=pod
+
+=item picture
+
+This is currently read-only.
+
 =back
 
 =head1 OPTIONS
@@ -160,19 +210,27 @@ None currently.
 
 =over 4
 
-=item default_options
+=item B<default_options()>
 
 Returns the default options for the plugin.  
 
-=item set_tag
+=item B<set_tag()>
 
 Save object back to FLAC header.
 
-=item get_tag
+=item B<get_tag()>
 
 Load information from FLAC header.
 
-=item close
+=item B<set_values>
+
+A list of values that can be set by this module.
+
+=item B<saved_values>
+
+A list of values that can be saved by this module.
+
+=item B<close()>
 
 Close the file and destroy the Audio::FLAC::Header
 
@@ -184,12 +242,20 @@ Returns the Audio::FLAC::Header object
 
 =head1 BUGS
 
-Plugin does not fully support all fields I would like, such as an APIC frame.
+Plugin does not fully support all fields I would like.  Pictures are read only (limitation of Audio::FLAC::Header).
 
 =head1 SEE ALSO 
 
 L<Audio::FLAC::Header>, L<Music::Tag>, L<Music::Tag::Amazon>, L<Music::Tag::File>, L<Music::Tag::Lyrics>,
 L<Music::Tag::M4A>, L<Music::Tag::MP3>, L<Music::Tag::MusicBrainz>, L<Music::Tag::OGG>, L<Music::Tag::Option>
+
+=head1 SOURCE
+
+Source is available at github: L<http://github.com/riemann42/Music-Tag-FLAC|http://github.com/riemann42/Music-Tag-FLAC>.
+
+=head1 BUG TRACKING
+
+Please use github for bug tracking: L<http://github.com/riemann42/Music-Tag-FLAC/issues|http://github.com/riemann42/Music-Tag-FLAC/issues>.
 
 =head1 AUTHOR 
 
@@ -197,16 +263,31 @@ Edward Allen III <ealleniii _at_ cpan _dot_ org>
 
 =head1 LICENSE
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the Artistic License, distributed
-with Perl.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either:
+
+a) the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+b) the "Artistic License" which comes with Perl.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either
+the GNU General Public License or the Artistic License for more details.
+
+You should have received a copy of the Artistic License with this
+Kit, in the file named "Artistic".  If not, I'll be glad to provide one.
+
+You should also have received a copy of the GNU General Public License
+along with this program in the file named "Copying". If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA or visit their web page on the Internet at
+http://www.gnu.org/copyleft/gpl.html.
+
 
 =head1 COPYRIGHT
 
 Copyright (c) 2007,2008 Edward Allen III. Some rights reserved.
 
-=cut
-
-1;
-
-# vim: tabstop=4
